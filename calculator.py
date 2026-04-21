d13ea314614e8f32191f6501fa53a3ff079b6de0
coursFlorian1 <coursFlorian1@proton.me>
2026-04-21 10:44:15 +0200

    Feat calculateur distance

--- calculator.py
+++ calculator.py
@@ -3,3 +3,8 @@ def calculer_prix(base, taxe):
 
 print(f"Prix final : {calculer_prix(100, 1.2)}")
 
+def calculer_distance(x1,y1,x2,y2):
+    return ((x2-x1)**2 + (y2-y1)**2)**0.5
+
+print(f"Distance entre mes deux points : {calculer_distance(1,2,4,6)}")
+
